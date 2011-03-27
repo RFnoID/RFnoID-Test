c1f97f97f0938df2e2d1f8423cd92fbe8ec5bb4b
RFNOID group <rfnoid@gmail.com>
2011-03-27 17:48:37 -0400

    hey, at least something works

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 #! /usr/bin/python
 from gnuradio import gr, gru, audio, eng_notation, window
 from gnuradio.eng_option import eng_option
-import sys
+from gnuradio.qtgui import qtgui
+from PyQt4 import QtGui
+import sys, sip
 # gui
-from gnuradio.wxgui import stdgui2, fftsink2, waterfallsink2, scopesink2, form, slider
-from optparse import OptionParser
-import wx
+# from gnuradio.wxgui import stdgui2, fftsink2, waterfallsink2, scopesink2, form, slider
+# from optparse import OptionParser
+# import wx
 
 class our_test(gr.top_block):
     def __init__(self):
@@ -15,37 +17,48 @@ class our_test(gr.top_block):
         self.qapp = QtGui.QApplication(sys.argv)
 
         sample_rate = 48000
-        ampl = 0.1
+        ampl = 1
         fft_size = 256
-        freq1 = 350
+        freq1 = 3500
         freq2 = 440
-        src0 = gr.sig_source_f (sample_rate, gr.GR_SIN_WAVE, freq1, ampl)
-        src1 = gr.sig_source_f (sample_rate, gr.GR_SIN_WAVE, freq2, ampl)
+        self.src0 = gr.sig_source_c (sample_rate, gr.GR_SIN_WAVE, freq1, ampl)
+        self.nse = gr.noise_source_c(gr.GR_GAUSSIAN, 0.1)
+        #src1 = gr.sig_source_f (sample_rate, gr.GR_SIN_WAVE, freq2, ampl)
         #fft0 = src0
         #fft1 = src1
-        thr = gr.throttle(gr.sizeof_gr_complex, 100*fftsize)
-        snk = qtgui.sink_c(fftsize, gr.firdes.WIN_BLACKMAN_hARRIS)
+        self.add = gr.add_cc()
+        self.thr = gr.throttle(gr.sizeof_gr_complex, 100*fft_size)
+        self.snk = qtgui.sink_c(fft_size, gr.firdes.WIN_BLACKMAN_hARRIS)
         mywindow = window.blackmanharris(fft_size)
         fft = gr.fft_vfc(fft_size, True, mywindow)
         power = 0
         for tap in mywindow:
             power += tap*tap
 
-        fdst = gr.file_sink (1024, "hello.out")
+        #fdst = gr.file_sink (1024, "hello.out")
         dst = audio.sink (sample_rate, "")
-        self.connect (src0, (dst, 0))
-        self.connect (src1, (dst, 1))
+        # self.connect (src0, (dst, 0))
+        # self.connect (src1, (dst, 1))
         stv = gr.stream_to_vector (gr.sizeof_float, fft_size)
         c2m = gr.complex_to_mag_squared (fft_size)
-        self.connect (src0, stv, fft, c2m, fdst)
-        print repr(c2m)
+        #self.connect (dst, stv, fft, c2m, fdst)
+        self.connect(self.src0, (self.add, 0))
+        self.connect(self.nse, (self.add, 1))
+        self.connect(self.add, self.thr, self.snk)
+        # Tell the sink we want it displayed
+        self.pyobj = sip.wrapinstance(self.snk.pyqwidget(), QtGui.QWidget)
+        self.pyobj.show()
+
+
+def main():
+    ot = our_test()
+    ot.start()
+    #raw_input('Press Enter to quit:')    tb = my_tb()
+    ot.qapp.exec_()
 
 if __name__ == '__main__':
     try:
-        ot = our_test()
-        ot.start()
-        raw_input('Press Enter to quit:')
-        ot.stop()
+        main()
     except KeyboardInterrupt:
         print "whoops"
 
