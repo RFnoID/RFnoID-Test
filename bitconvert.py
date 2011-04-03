1e721ff191c09d01ad11b69f815e5ad9c2ff5a43
RFNOID group <rfnoid@gmail.com>
2011-04-03 16:42:57 -0400

    finally working

--- bitconvert.py
+++ bitconvert.py
@@ -3,7 +3,7 @@ from gnuradio.eng_option import eng_option
 import wave
 
 class converter:
-    def init( self, sr=1e7, t2=2, delay=100 ):
+    def __init__( self, sr=1e7, t2=2, delay=100 ):
         self.sample_rate = sr
         self.sr = sr/1e6 # should be 10 by default
         self.t2 = t2
@@ -14,9 +14,9 @@ class converter:
     def convert( self, val ):
         print "Using",self.sr,"samples per microsecond"
         if val == 52:
-            return make_52( 100 )
+            return self.make_52( )
         elif val == 26:
-            return make_26( 100 )
+            return self.make_26( )
 
     # seq_x( t2, sampling rate )
     # first part = [high]*(sampling rate)*4.72 microseconds times
@@ -24,21 +24,21 @@ class converter:
     # third part = [high]*(sampling rate)*(4.72-t2) # 9.44-4.72-t2
     # returns list of first second and third parts
     def x( self ):
-        return self.high*round(self.sr*4.72) + \
-            self.low*round(self.sr*self.t2) + \
-            self.high*round(self.sr*(4.72-self.t2))
+        return self.high*int(round(self.sr*4.72)) + \
+            self.low*int(round(self.sr*self.t2)) + \
+            self.high*int(round(self.sr*(4.72-self.t2)))
 
     # seq_y( sampling rate )
     # returns [high]*(sampling rate)*9.44
     def y( self ):
-        return self.high*self.sr*9.44
+        return self.high*int(round(self.sr*9.44))
 
     # seq_z( t2, sampling rate )
     # first part = [low]*(sampling rate)/t2
     # second part = [high]*(sampling rate)/(9.44-t2)
     def z( self ):
-        return self.low*self.sr*self.t2 + \
-            self.high*self.sr*(9.44-self.t2)
+        return self.low*int(round(self.sr*self.t2)) + \
+            self.high*int(round(self.sr*(9.44-self.t2)))
 
     # make_52( delay=100 )
     # returns Z Z X Y Z X Y X [Y]*delay
@@ -54,7 +54,7 @@ class converter:
             self.y() + self.z() + self.x() + self.y() + \
             self.z() + self.y()*self.delay
 
-if "__name__" == __main__():
+if __name__ == '__main__':
     try:
         f52 = wave.open("wave52.wav","w")
         f26 = wave.open("wave26.wav","w")
@@ -63,11 +63,23 @@ if "__name__" == __main__():
     
     # instantiate the converter class
     conv = converter(1e7, 2, 100)
+    s52 = conv.convert(52)
+    s26 = conv.convert(26)
 
     # make the wave header
-    f52.setframerate(conv.sample_rate)
-    f26.setframerate(conv.sample_rate)
+    # The tuple should be (nchannels, sampwidth, 
+    # framerate, nframes, comptype, compname), 
+    # with values valid for the set*() methods.
+    # Sets all parameters.
+    f52.setparams((1, 2, conv.sample_rate, \
+                       len(s52), 'NONE', 'NONE'))
+    f26.setparams((1, 2, conv.sample_rate, \
+                       len(s26), 'NONE', 'NONE'))
 
     # write audio frames, without correcting nframes
-    f52.writeframesraw(conv.convert(52))
-    f26.writeframesraw(conv.convert(26))
+    f52.writeframesraw( str(s52) )
+    f26.writeframesraw( str(s26) )
+
+    # done
+    f52.close()
+    f26.close()
