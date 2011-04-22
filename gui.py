eaa09d68d401337a789ff19f95fb2a32f09c56a5
RFNOID group <rfnoid@gmail.com>
2011-04-22 13:34:30 -0400

    configured gui.py and made it not crash

--- gui.py
+++ gui.py
@@ -1,15 +1,18 @@
+#! /usr/bin/python
 import subprocess, Tkinter, tkFont
+import re
 from graphics import *
 
 master = Tkinter.Tk()
 
 def check():
     tmp = subprocess.Popen('nfc-list', stdout = subprocess.PIPE)
-    x = tmp.communicate()
-    print x
+    x = str(tmp.communicate())
     win = GraphWin()
     shape = Rectangle(Point(150,150), Point(50,50))
-    if len(x) > 100:
+    if "UID" in x:
+        print "Read",re.search("UID",x).group(0)
+    if len(x) > 400:
         color = "green"
     else:
         color = "red"
@@ -17,18 +20,21 @@ def check():
     shape.setFill(color)
     shape.draw(win)
     for i in range(2):
-        win.getMouse()
+        try:
+            win.getMouse()
+        except GraphicsError:
+            raise SystemExit
     win.close()   
 
 def main():
     customFont = tkFont.Font(family="system",size=16,weight="bold")
     button = Tkinter.Button(master,text="Start",command=check,font=customFont)
     button.pack(fill='both', expand=1, padx=20, pady=20)
-    master.mainloop()
-
-if __name__ == '__main__':
     try:
-        main()
-    except:
+        master.mainloop()
+    except KeyboardInterrupt:
         print "Done"
         raise SystemExit
+
+if __name__ == '__main__':
+    main()
